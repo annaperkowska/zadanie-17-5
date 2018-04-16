56f308031296068484db2f04d35a47661da0ba10
Anna Perkowska <perkowska.a@gmail.com>
2018-04-16 10:06:38 +0200

    Upadte server, delete files

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('assets'));
+app.use('/store', function(req, res, next) {
+	console.log('Hej, jestem pośrednikiem przy żądaniu do /store');
+	next();
+});
 
 app.get('/', function (req, res) {
-    res.sendFile('/index.html');
+    res.send('Hello world!');
     
 });
 
-app.get('/userform', function (req, res) {
-	const response = {
-		first_name: req.query.first_name,
-		last_name: req.query.last_name
-	};
-    res.end(JSON.stringify(response));
+app.get('/store', function(req, res) {
+	res.send('To jest sklep');
 });
 
 var server = app.listen(3000, 'localhost', function() {
